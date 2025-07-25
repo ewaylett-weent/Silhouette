b9dce2017c880691bf124aaa1bd3bc5536be1bcb
ewaylett-wente <ewaylettgames@gmail.com>
2025-07-25 15:38:20 -0400

    gaea and mapGen
    added gaea addon
    created map generator

--- Scripts/village.gd
+++ Scripts/village.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$buildings/Portal.destination = "res://Scenes/silhouette.tscn"
+	$buildings/Portal.destination = "res://Scenes/newLevel.tscn"
 	$buildings/Portal._change_Text("Press 'F' to Travel to the Void")
 
 
