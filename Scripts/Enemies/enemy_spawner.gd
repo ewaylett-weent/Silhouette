4511c1eb60c50a007c6e567405a2d1c93ba0253d
ewaylett-wente <ewaylettgames@gmail.com>
2025-11-26 14:31:45 -0500

    spawing update
    Updated with initial spawn and respawn, group variable

--- Scripts/Enemies/enemy_spawner.gd
+++ Scripts/Enemies/enemy_spawner.gd
@@ -1,18 +1,52 @@
 extends Node2D
 
 var enemy = preload("res://Scenes/Enemies/enemy.tscn")
-var spawnedenemies = 0
+var spawnedEnemies = false
+var groupSize = 2
+var enemyArray = []
 
 func _ready() -> void:
 	print("setting up spawner")
-
+	spawnEnemies()
+	
 func _on_timer_timeout() -> void:
+	print("timer timeout")
+	spawnEnemies()
 	
-	if spawnedenemies < 3:
-		print("spawning enemy")
-		var ene = enemy.instantiate()
-		ene.position = position
-		get_parent().add_child(ene)
-		spawnedenemies += 1
+func spawnEnemies() : 
+	print(enemyArray.size())
+	var i = 0
+	var l = 0
 	
+	while l < enemyArray.size():
+		print("checking validity")
+		if is_instance_valid(enemyArray.get(l)) == false:
+			print("node gone")
+			enemyArray.remove_at(l)
+		if enemyArray.is_empty():
+			spawnedEnemies = false
+		l += 1
 	
+	if spawnedEnemies == false:
+		while i < groupSize:
+			print("spawning enemy")
+			var ene = enemy.instantiate()
+			ene.position = getSpawnPosition()
+			get_parent().add_child.call_deferred(ene)
+			enemyArray.append(ene)
+			print(enemyArray.size())
+			i += 1
+		
+		spawnedEnemies = true
+	
+func getSpawnPosition() : 
+	var center_position = position
+	var min_distance = -200
+	var max_distance = 200
+	
+	var newPosition = center_position
+	newPosition.x = center_position.x + randf_range(min_distance, max_distance)
+	newPosition.y = center_position.y + randf_range(min_distance, max_distance)
+	
+	print(newPosition)
+	return newPosition
