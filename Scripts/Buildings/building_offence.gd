917de8daaca2ef8265667795d73cf143995dc61f
ewaylett-wente <ewaylettgames@gmail.com>
2025-11-28 14:04:21 -0500

    Offence Building Leveling
    added leveling to offence building with added text

---
+++ Scripts/Buildings/building_offence.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+var level = 1
+var playerInArea = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$LevelText.clear()
+	$LevelText.add_text("Level : " + str(level))
+	$UpgradeText.clear()
+	$UpgradeText.add_text("Press F to Upgrade to Level  " + str(level + 1))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact") and playerInArea:
+		LevelBuilding()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		$UpgradeText.visible = true
+		playerInArea = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		$UpgradeText.visible = false
+		playerInArea = false
+	
+func LevelBuilding() : 
+	level += 1
+	$LevelText.clear()
+	$LevelText.add_text("Level : " + str(level))
+	$UpgradeText.clear()
+	$UpgradeText.add_text("Press F to Upgrade to Level  " + str(level + 1))
